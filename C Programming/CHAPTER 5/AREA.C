6c12ccfa53da33590f87ea7621b651d689465376
Abhishek Singh <112766441+Abhisheksingh0303@users.noreply.github.com>
2022-09-04 13:11:04 +0530

    Add files via upload

---
+++ C Programming/CHAPTER 5/AREA.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+clrscr();
+int length,breadth;
+printf("enter the length \n");
+scanf("%d",&length);
+printf("enter the breadth\n");
+scanf("%d",&breadth);
+printf("the area of rectangle is\n");
+printf("%d",length*breadth);
+getch();
+}
