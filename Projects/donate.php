25aefd78ea482a5dddef5c0c96ad93c483a266a4
Abhishek Singh <112766441+Abhisheksingh0303@users.noreply.github.com>
2023-04-29 21:33:12 +0530

    Add files via upload

---
+++ Projects/donate.php
@@ -0,0 +1,48 @@
+<?php include 'include/header.php' ?>
+
+<section class="page-header">
+            <div class="page-header-bg" style="background-image: url(assets/images/backgrounds/page-header-bg.jpg)">
+            </div>
+            <div class="container">
+                <div class="page-header__inner3">
+                <h2>Donate Us</h2>
+                    <ul class="thm-breadcrumb list-unstyled">
+                        <li><a href="index.php">Home</a></li>
+                        <li><span>/</span></li>
+                        <li>Donate</li>
+                    </ul>
+                 
+                </div>
+            </div>
+        </section>
+
+
+
+
+
+        <div class="card">
+  <h2>Account Details For Donation</h2>
+  <p>
+  <span>Bank Name :</span>
+  "PUNJAB NATIONAL BANK, RAJMAI"
+  </p>
+
+  <p>
+  <span1>Account Number :</span1>
+  "1259010315456"
+  </p>
+
+  <p>
+  <span2>IFSC Code</span2>
+  "PUNB0125920"
+  </p>
+
+  <p>
+  <span3>Account Holder Name :</span3>
+  "Pathikrit"
+  </p>
+
+</div>
+
+
+<?php include 'include/footer.php' ?>
